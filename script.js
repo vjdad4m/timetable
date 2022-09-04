4693bcbd8911e1cd048d53e92d45f50543f078cb
Adam Vajda <vjdad4m@gmail.com>
2022-09-04 19:08:03 +0200

    change

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-const days = ['Monday', 'Tuesday', 'Wednesday', 'Thrusday', 'Friday', 'Saturday', 'Sunday'];
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const gifs = ['https://media.giphy.com/media/RgnTXvE24wFjUhB3Dt/giphy.gif',
             'https://media.giphy.com/media/T1zgJ7cp8tWla/giphy.gif',
             'https://media.giphy.com/media/F3BeiZNq6VbDwyxzxF/giphy.gif',
             'https://media.giphy.com/media/3o6Zt62PeJeFUDwBUI/giphy.gif',
             'https://media.giphy.com/media/TfWhFbURIirNegNN4t/giphy.gif'];
 
-
-            
+       
 import timetable from './timetable.json' assert {type: 'json'};
 
 function update() {
@@ -21,18 +20,20 @@ function update() {
     var h = document.getElementById('day');
     var t = document.getElementById('time');
     var c = document.getElementById('class');
+    var n = document.getElementById('next');
     var p = document.getElementById('gif');
 
-    h.innerText = days[day]; 
+    h.innerText = days[day];
     t.innerText = hrs + ':' + min + ':' + sec
     p.innerHTML = '<img width=420 height=420 src=".">'.replace('.', randomGif);
 
     let msg = '';
+    let next = '';
 
-    if (hrs < 6) {
+    if (hrs < 5) {
         msg = '<i>Bro go to sleep...</i>';    
     } else {
-        if (day > 4) {
+        if (day == 0 | day == 7) {
             msg = '<i>Dude it is the weekend...</i>'
         }
         else {
@@ -44,10 +45,17 @@ function update() {
                     msg = '<i>School is over!</i>';
                 }
             }
+            next = timetable[day][hrs+1]
+            if (next == undefined) {
+                next = '<i>School is over!</i>'
+            }
         }
     }
 
-    c.innerHTML = msg.replaceAll('\n', '<br>');
+    c.innerHTML = '<span>Currently:</span><br>' + msg.replaceAll('\n', '<br>');
+    if (next != '' && next != undefined) {
+        n.innerHTML = '<span>Next up:</span><br>' + next.replaceAll('\n', '<br>');
+    }
 
 }
 
